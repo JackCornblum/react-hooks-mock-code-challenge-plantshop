d11157983bb35ece2080f96373b182c5627fa98a
jackcornblum <jcornblum@gmail.com>
2021-06-15 12:12:57 -0400

    completed core deliverables

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantData, setPlantData}) {
+  console.log(plantData)
+  let plantArr = plantData.map(obj => {
+    return <PlantCard name={obj.name} img={obj.image} price={obj.price} key={obj.id} />
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantArr}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,71 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantData, setPlantData] = useState([])
+  const [reload, setReload] = useState(false)
+  const [searchValue, setSearchValue] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(data => setPlantData(data))
+  }, [reload])
+
+  function addNewPlant(e){
+    let plantName = e.target[0].value
+    let plantUrl = e.target[1].value
+    let plantPrice = e.target[2].value
+    e.preventDefault()
+    // console.log(e)
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        name: plantName,
+        image: plantUrl,
+        price: plantPrice
+      })
+    })
+    
+    setReload(!reload)
+    
+  }
+
+  function filterPlants(e){
+    
+    if(e.target.value !== "") {
+      let copyOfPlantData = [...plantData]
+      let filteredList = copyOfPlantData.filter(plant => {
+        const name = plant.name.toLowerCase()
+        const query = e.target.value.toLowerCase()
+        return name.includes(query)
+
+      })
+      setPlantData(filteredList)
+    }
+    else {
+      setReload(!reload)
+    }
+
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm
+        addNewPlant={addNewPlant}
+      />
+      <Search 
+        filterPlants={filterPlants}
+      />
+      <PlantList 
+        plantData={plantData}
+        setPlantData={setPlantData}
+      />
     </main>
   );
 }
